2d1403cc93035ba0726c9343648391de2d0347f0
layla  hammoud <laylahammoud63@gmail.com>
2021-01-01 13:44:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+function saturdayFun(play="roller-skate"){
+  return(`This Saturday, I want to ${play}!`);
+}
+function mondayWork(work="go to the office"){
+  return(`This Monday, I will ${work}.`)
+}
+function wrapAdjective(string="*"){
+  return function(gift="special"){
+    return `You are ${string}${gift}${string}!`
+  }
+
+}
+let Calculator= {
+  add: function (a,b) {return a+b},
+  subtract: function (a,b) {return a-b},
+  multiply: function (a,b) {return a*b},
+  divide:function(a,b) {return a/b},
+
+  }
+function actionApplyer(integer,array){
+  let theNumber=integer
+
+    for (let i = 0; i < array.length; i++ ){
+   theNumber = array[i](theNumber)
+  }
+  return theNumber
+}
